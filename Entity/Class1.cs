4367f33504c931431d15b485c62d03b2f5be0401
andres pertuz <andrespertuz16@hotmail.com>
2021-04-22 02:45:36 -0500

    Confirmo cambio de Framework

--- Entity/Class1.cs
+++ Entity/Class1.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Entity
 {
-    public class Propietario : Persona
+    public class Class1
     {
-        public double CodPropietario { get; set; }
-
     }
 }
--- PresentacionGUI/Program.cs
+++ PresentacionGUI/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,12 +9,11 @@ namespace PresentacionGUI
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
