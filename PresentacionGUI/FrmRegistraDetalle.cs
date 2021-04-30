f3873e0443520de953a576ddee722ed0f52ca0c4
andres pertuz <andrespertuz16@hotmail.com>
2021-04-29 19:09:29 -0500

    Prueba

--- PresentacionGUI/FrmRegistraDetalle.cs
+++ PresentacionGUI/FrmRegistraDetalle.cs
@@ -16,6 +16,7 @@ namespace PresentacionGUI
         {
             InitializeComponent();
         }
+
     }
 }
 //<div>Iconos diseñados por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
