2b84ca85e66c68c20c67af4d5488957272667812
Fdamirez <fdramirez@unicesar.edu.co>
2021-06-27 13:34:05 -0500

    Nueva base de Datos

---
+++ SQLQuery1.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE Personas (
+    Identificacion VARCHAR PRIMARY KEY NOT NULL,
+    Rol VARCHAR,
+    PrimerNombre VARCHAR,
+    SegundoNombre VARCHAR,
+    PrimerApellido VARCHAR,
+    SegundoApellido VARCHAR,
+    NumeroContacto VARCHAR
+);
+
+
+
+
+CREATE TABLE Taxis (
+    Placa VARCHAR(15) PRIMARY KEY NOT NULL,
+    Modelo VARCHAR(10),
+    Kilometraje VARCHAR(20),
+    IdPropietario VARCHAR(20),
+    IdConductor VARCHAR(20),
+    CONSTRAINT fk_Persona_IdPropietario FOREIGN KEY (IdPropietario) REFERENCES Personas (Identificacion),
+    CONSTRAINT fk_Persona_IdConductor FOREIGN KEY (IdConductor) REFERENCES Personas (Identificacion)
+);
+
+
+CREATE TABLE Detalles (
+    CodigoDetalle VARCHAR(10) PRIMARY KEY NOT NULL,
+    TipoDetalle VARCHAR(10),
+    FechaDetalle DATETIME,
+    ValorDetalle DECIMAL,
+    Descripcion VARCHAR(10),
+    Placa VARCHAR(15),
+    CONSTRAINT fk_Detalles_Placa FOREIGN KEY (Placa) REFERENCES Taxis (Placa)
+);
